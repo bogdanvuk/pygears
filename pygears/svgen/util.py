0bc0e490c0ac8ebc9a22f6abcc99dc12ffd79399
Bogdan Vukobratovic <bogdan.vukobratovic-ext@frobas.com>
2018-05-26 11:45:13 +0200

    Add generic sv typedef generator

--- pygears/svgen/util.py
+++ pygears/svgen/util.py
@@ -1,3 +1,7 @@
+from pygears.typing.visitor import TypingVisitorBase
+from pygears.typing import bitw
+
+
 def svgen_visitor(cls):
     def svgen_action(top, conf):
         v = cls()
@@ -6,3 +10,163 @@ def svgen_visitor(cls):
         return top
 
     return svgen_action
+
+
+class SVGenTypeVisitor(TypingVisitorBase):
+    struct_str = "typedef struct packed {"
+    union_str = "typedef union packed {"
+
+    def __init__(self, name, basic_type='logic'):
+        self.struct_array = []
+        self.context = name
+        self.basic_type = basic_type
+        self.depth = -1
+
+    def visit(self, type_, field):
+        self.depth += 1
+        type_declaration = super().visit(type_, field)
+        self.depth -= 1
+        return type_declaration
+
+    def visit_int(self, type_, field):
+        if (not self.depth):
+            self.struct_array.append(
+                f'typedef {self.basic_type} [{int(type_)-1}:0] {self.context}_t; // {type_}\n'
+            )
+        return f'{self.basic_type} [{int(type_)-1}:0]'
+
+    def visit_bool(self, type_, field):
+        if (not self.depth):
+            self.struct_array.append(
+                f'typedef {self.basic_type} [0:0] {self.context}_t; // {type_}'
+            )
+        return f'{self.basic_type} [0:0]'
+
+    def visit_uint(self, type_, field):
+        if (int(type_) == 0):
+            return None
+        if (not self.depth):
+            self.struct_array.append(
+                f'typedef {self.basic_type} [{int(type_)-1}:0] {self.context}_t; // {type_}\n'
+            )
+        return f'{self.basic_type} [{int(type_)-1}:0]'
+
+    def visit_unit(self, type_, field):
+        return None
+
+    def visit_union(self, type_, field):
+        struct_fields = []
+        max_len = 0
+        high_parent_context = self.context
+        middle_parent_context = f'{high_parent_context}_data'
+        low_parent_context = f'{middle_parent_context}_data'
+
+        for t in (type_.types()):
+            if (int(t) > max_len):
+                max_len = int(t)
+
+        # for t, f in zip(reversed(type_.args), reversed(type_.fields)):
+        #     middle_type = Tuple[{f: t, 'dummy': Uint[max_len-int(t)]}]
+        #     struct_fields.append(f'{self.struct_str}')
+        #     for tp, fd in zip(reversed(middle_type.args), reversed(middle_type.fields)):
+        #         self.context = f'{low_parent_context}_{fd}'
+        #         type_declaration = self.visit(tp, fd)
+        #         if (type_declaration is not None):
+        #             struct_fields.append(f'    {type_declaration} {fd}; // {tp}')
+        #     struct_fields.append(f'}} {middle_parent_context}_{f}_t;\n')
+
+        for t, f in zip(reversed(type_.args), reversed(type_.fields)):
+            struct_fields.append(f'{self.struct_str} // ({t}, u?)')
+            self.context = f'{low_parent_context}_{f}'
+            type_declaration = self.visit(t, f)
+            if (int(t) < max_len):
+                struct_fields.append(
+                    f'    {self.basic_type} [{max_len-int(t)-1}:0] dummy; // u{max_len-int(t)}'
+                )
+            if (type_declaration is not None):
+                struct_fields.append(f'    {type_declaration} {f}; // {t}')
+            struct_fields.append(f'}} {middle_parent_context}_{f}_t;\n')
+
+        # create union
+        struct_fields.append(f'{self.union_str} // {type_}')
+        for t, f in zip(reversed(type_.args), reversed(type_.fields)):
+            struct_fields.append(
+                f'    {middle_parent_context}_{f}_t {f}; // ({t}, u?)')
+        struct_fields.append(f'}} {middle_parent_context}_t;\n')
+
+        # create struct
+        struct_fields.append(f'{self.struct_str} // {type_}')
+        struct_fields.append(
+            f'    {self.basic_type} [{bitw(len(type_.args)-1)-1}:0] ctrl; // u{bitw(len(type_.args)-1)}'
+        )
+        struct_fields.append(f'    {middle_parent_context}_t data; // {type_}')
+        struct_fields.append(f'}} {high_parent_context}_t;\n')
+
+        self.struct_array.append('\n'.join(struct_fields))
+
+        self.context = high_parent_context
+
+        return f'{high_parent_context}_t'
+
+    def visit_queue(self, type_, field):
+        struct_fields = []
+        parent_context = self.context
+        struct_fields.append(f'{self.struct_str} // {type_}')
+        struct_fields.append(
+            f'    {self.basic_type} [{type_.args[1]-1}:0] eot; // u{type_.args[1]}'
+        )
+        self.context = f'{parent_context}_data'
+        type_declaration = self.visit(type_.args[0], type_.fields[0])
+
+        if (type_declaration is not None):
+            struct_fields.append(
+                f'    {type_declaration} data; // {type_.args[0]}')
+
+        struct_fields.append(f'}} {parent_context}_t;\n')
+        self.struct_array.append('\n'.join(struct_fields))
+
+        self.context = parent_context
+
+        return f'{parent_context}_t'
+
+    def visit_tuple(self, type_, field):
+        struct_fields = []
+        parent_context = self.context
+        struct_fields.append(f'{self.struct_str} // {type_}')
+        for t, f in zip(reversed(type_.args), reversed(type_.fields)):
+            self.context = f'{parent_context}_{f}'
+            type_declaration = self.visit(t, f)
+            struct_fields.append(f'    {type_declaration} {f}; // {t}')
+
+        struct_fields.append(f'}} {parent_context}_t;\n')
+        self.struct_array.append('\n'.join(struct_fields))
+
+        self.context = parent_context
+
+        return f'{parent_context}_t'
+
+    def visit_array(self, type_, field):
+        if (not self.depth):
+            struct_fields = []
+            parent_context = self.context
+            self.context = f'{parent_context}_data'
+
+        type_declaration = self.visit(type_.args[0], type_.fields[0])
+        split_type = type_declaration.split(" ", 1)
+        split_type.insert(1, f'[{type_.args[1]-1}:0]')
+        type_declaration = ' '.join(split_type)
+
+        if (not self.depth):
+            struct_fields.append(
+                f'typedef {type_declaration} {parent_context}_t; // {type_}\n')
+            self.struct_array.append('\n'.join(struct_fields))
+            self.context = parent_context
+            return f'{parent_context}_t'
+        else:
+            return f'{type_declaration}'
+
+
+def svgen_typedef(dtype, name):
+    vis = SVGenTypeVisitor(name)
+    vis.visit(type_=dtype, field=name)
+    return '\n'.join(vis.struct_array)
---
+++ tests/svgen/test_typedef.py
@@ -0,0 +1,214 @@
+from pygears.svgen.util import svgen_typedef
+from pygears.typing import Uint, Int, Bool, Tuple, Queue, Union, Array, Unit
+from utils import equal_on_nonspace
+
+
+def test_uint():
+    test_ref = "typedef logic [15:0] data_t; // u16"
+    svtype = svgen_typedef(Uint[16], 'data')
+
+    assert equal_on_nonspace(svtype, test_ref)
+
+
+def test_int():
+
+    test_ref = "typedef logic [15:0] data_t; // i16"
+    svtype = svgen_typedef(Int[16], 'data')
+
+    assert equal_on_nonspace(svtype, test_ref)
+
+
+def test_array():
+    test_ref = "typedef logic [1:0] [15:0] data_t; // Array[u16, 2]"
+    svtype = svgen_typedef(Array[Uint[16], 2], 'data')
+
+    assert equal_on_nonspace(svtype, test_ref)
+
+
+def test_multiarray():
+    test_ref = "typedef logic [3:0] [1:0] [15:0] data_t; // Array[Array[u16, 2], 4]"
+    svtype = svgen_typedef(Array[Array[Uint[16], 2], 4], 'data')
+
+    assert equal_on_nonspace(svtype, test_ref)
+
+
+def test_tuple():
+    test_ref = """
+typedef struct packed { // (u1, u2)
+    logic [1:0] f1; // u2
+    logic [0:0] f0; // u1
+} data_t;
+"""
+    svtype = svgen_typedef(Tuple[Uint[1], Uint[2]], 'data')
+
+    assert equal_on_nonspace(svtype, test_ref)
+
+
+def test_tuple_multilevel():
+    test_ref = """
+typedef struct packed { // (u3, u4)
+    logic [3:0] f1; // u4
+    logic [2:0] f0; // u3
+} data_f1_t;
+
+typedef struct packed { // (u1, u2)
+    logic [1:0] f1; // u2
+    logic [0:0] f0; // u1
+} data_f0_t;
+
+typedef struct packed { // ((u1, u2), (u3, u4))
+    data_f1_t f1; // (u3, u4)
+    data_f0_t f0; // (u1, u2)
+} data_t;
+"""
+    svtype = svgen_typedef(
+        Tuple[Tuple[Uint[1], Uint[2]], Tuple[Uint[3], Uint[4]]], 'data')
+
+    assert equal_on_nonspace(svtype, test_ref)
+
+
+def test_namedtuple():
+    test_ref = """
+typedef struct packed { // (u1, u2)
+    logic [1:0] field2; // u2
+    logic [0:0] field1; // u1
+} data_t;
+"""
+    svtype = svgen_typedef(Tuple[{
+        'field1': Uint[1],
+        'field2': Uint[2]
+    }], 'data')
+
+    assert equal_on_nonspace(svtype, test_ref)
+
+
+def test_queue():
+    test_ref = """
+typedef struct packed { // [u16]
+    logic [0:0] eot; // u1
+    logic [15:0] data; // u16
+} data_t;
+"""
+    svtype = svgen_typedef(Queue[Uint[16]], 'data')
+
+    assert equal_on_nonspace(svtype, test_ref)
+
+
+def test_multiqueue():
+    test_ref = """
+typedef struct packed { // [u16]
+    logic [5:0] eot; // u1
+    logic [1:0] data; // u16
+} data_t;
+"""
+    svtype = svgen_typedef(Queue[Uint[2], 6], 'data')
+
+    assert equal_on_nonspace(svtype, test_ref)
+
+
+def test_union():
+    test_ref = """
+typedef struct packed { // (u2, u?)
+    logic [1:0] alt2; // u2
+} data_data_alt2_t;
+
+typedef struct packed { // (u1, u?)
+    logic [0:0] dummy; // u1
+    logic [0:0] alt1; // u1
+} data_data_alt1_t;
+
+typedef union packed { // u1 | u2
+    data_data_alt2_t alt2; // (u2, u?)
+    data_data_alt1_t alt1; // (u1, u?)
+} data_data_t;
+
+typedef struct packed { // u1 | u2
+    logic [0:0] ctrl; // u1
+    data_data_t data; // u1 | u2
+} data_t;
+"""
+    svtype = svgen_typedef(Union[{'alt1': Uint[1], 'alt2': Uint[2]}], 'data')
+
+    assert equal_on_nonspace(svtype, test_ref)
+
+
+def test_union_unit():
+    test_ref = """
+typedef struct packed { // ((), u?)
+    logic [15:0] dummy; // u16
+} data_data_alt2_t;
+
+typedef struct packed { // (u16, u?)
+    logic [15:0] alt1; // u16
+} data_data_alt1_t;
+
+typedef union packed { // u16 | ()
+    data_data_alt2_t alt2; // ((), u?)
+    data_data_alt1_t alt1; // (u16, u?)
+} data_data_t;
+
+typedef struct packed { // u16 | ()
+    logic [0:0] ctrl; // u1
+    data_data_t data; // u16 | ()
+} data_t;
+"""
+    svtype = svgen_typedef(Union[{'alt1': Uint[16], 'alt2': Unit}], 'data')
+
+    assert equal_on_nonspace(svtype, test_ref)
+
+
+def test_complex():
+    test_ref = """
+typedef struct packed { // (u16, u16)
+    logic [15:0] f1; // u16
+    logic [15:0] f0; // u16
+} data_data_data_data_un2_data_field1_t;
+
+typedef struct packed { // (Array[Array[(u16, u16), 2], 4], u16)
+    logic [15:0] field2; // u16
+    data_data_data_data_un2_data_field1_t [3:0] [1:0] field1; // Array[Array[(u16, u16), 2], 4]
+} data_data_data_data_un2_data_t;
+
+typedef struct packed { // [(Array[Array[(u16, u16), 2], 4], u16)]^5
+    logic [4:0] eot; // u5
+    data_data_data_data_un2_data_t data; // (Array[Array[(u16, u16), 2], 4], u16)
+} data_data_data_data_un2_t;
+
+typedef struct packed { // (u16, u16)
+    logic [15:0] f1; // u16
+    logic [15:0] f0; // u16
+} data_data_data_data_un1_t;
+
+typedef struct packed { // ([(Array[Array[(u16, u16), 2], 4], u16)]^5, u?)
+    data_data_data_data_un2_t un2; // [(Array[Array[(u16, u16), 2], 4], u16)]^5
+} data_data_data_un2_t;
+
+typedef struct packed { // (Array[(u16, u16), 2], u?)
+    logic [212:0] dummy; // u213
+    data_data_data_data_un1_t [1:0] un1; // Array[(u16, u16), 2]
+} data_data_data_un1_t;
+
+typedef union packed { // Array[(u16, u16), 2] | [(Array[Array[(u16, u16), 2], 4], u16)]^5
+    data_data_data_un2_t un2; // ([(Array[Array[(u16, u16), 2], 4], u16)]^5, u?)
+    data_data_data_un1_t un1; // (Array[(u16, u16), 2], u?)
+} data_data_data_t;
+
+typedef struct packed { // Array[(u16, u16), 2] | [(Array[Array[(u16, u16), 2], 4], u16)]^5
+    logic [0:0] ctrl; // u1
+    data_data_data_t data; // Array[(u16, u16), 2] | [(Array[Array[(u16, u16), 2], 4], u16)]^5
+} data_data_t;
+
+typedef data_data_t [2:0] data_t; // Array[Array[(u16, u16), 2] | [(Array[Array[(u16, u16), 2], 4], u16)]^5, 3]
+"""
+
+    dtype_uint = Uint[16]
+    dtype_tpl = Tuple[dtype_uint, dtype_uint]
+    dtype_arr = Array[dtype_tpl, 2]
+    dtype_arr_arr = Array[dtype_arr, 4]
+    dtype_dict = Tuple[{'field1': dtype_arr_arr, 'field2': dtype_uint}]
+    dtype_q = Queue[dtype_dict, 5]
+    dtype_union = Union[{'un1': dtype_arr, 'un2': dtype_q}]
+    dtype_arr_union = Array[dtype_union, 3]
+    svtype = svgen_typedef(dtype_arr_union, 'data')
+
+    assert equal_on_nonspace(svtype, test_ref)
