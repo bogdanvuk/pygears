f16dc0d3ed702607aba3af12fd12d15c17a57ed1
Bogdan Vukobratovic <bogdan.vukobratovic@kortiq.com>
2018-06-15 09:54:45 +0200

    Start implementing bidir sim for batch_active

--- examples/sim/batch_active_sock.py
+++ examples/sim/batch_active_sock.py
@@ -1,14 +1,28 @@
-from pygears.sim import sim, verif, drv
+from pygears.sim import sim, verif, drv, mon
 from pygears.sim.modules.socket import SimSocket
 from pygears.sim.modules.seqr import seqr
 
 from pygearslib.batch_active import batch_active
 from pygears.typing import Queue, Uint
+from pygears import gear
 
 t_din = Queue[Uint[16]]
 
+
+@gear
+async def check(din, *, ret):
+    val = await din.get()
+    din.task_done()
+    ret.append(val)
+
+
+ret = []
 seqr(t=t_din, seq=[list(range(10))]) \
     | drv \
-    | batch_active(batch_size=2, sim_cls=SimSocket)
+    | batch_active(batch_size=2, sim_cls=SimSocket) \
+    | mon \
+    | check(ret=ret)
 
 sim()
+
+print(ret)
