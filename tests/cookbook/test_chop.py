d02c8190b1cc7095d104fdb6950202dc350e7e00
Andrea Erdeljan <andrea.erdeljan-ext@frobas.com>
2018-06-18 11:44:48 +0200

    chop socket test

--- tests/cookbook/test_chop.py
+++ tests/cookbook/test_chop.py
@@ -1,11 +1,13 @@
 from nose import with_setup
-from pygears import clear
-from pygears.sim import sim
-from pygears.sim.modules.verilator import SimVerilated
-from pygears.cookbook.verif import directed, verif
-from pygears.sim.modules.seqr import seqr
-from pygears.typing import Queue, Uint
+
+from pygears import clear, registry
 from pygears.cookbook.chop import chop
+from pygears.cookbook.verif import directed, verif
+from pygears.sim import sim
+from pygears.sim.modules.seqr import seqr
+from pygears.sim.modules.socket import SimSocket
+from pygears.sim.modules.verilator import SimVerilated
+from pygears.typing import Queue, Uint
 
 
 @with_setup(clear)
@@ -39,6 +41,31 @@ def test_verilator_cosim():
                                      list(range(3))]),
         seqr(t=Uint[16], seq=[2, 3]),
         f=chop(sim_cls=SimVerilated),
+        ref=chop(name='ref_model'))
+
+    sim()
+
+
+@with_setup(clear)
+def test_socket_sim():
+    directed(
+        seqr(t=Queue[Uint[16]], seq=[list(range(9)),
+                                     list(range(3))]),
+        seqr(t=Uint[16], seq=[2, 3]),
+        f=chop(sim_cls=SimSocket),
+        ref=[[[0, 1], [2, 3], [4, 5], [6, 7], [8]], [[0, 1, 2]]])
+
+    sim()
+
+
+@with_setup(clear)
+def test_socket_cosim():
+    registry('SimConfig')['dbg_assert'] = True
+    verif(
+        seqr(t=Queue[Uint[16]], seq=[list(range(9)),
+                                     list(range(3))]),
+        seqr(t=Uint[16], seq=[2, 3]),
+        f=chop(sim_cls=SimSocket),
         ref=chop(name='ref_model'))
 
     sim()
