ab471828e205dbb073d46e2d4ef4a060fd1367e5
Andrea Erdeljan <andrea.erdeljan-ext@frobas.com>
2018-06-18 14:24:16 +0200

    verilator test for trr

--- tests/cookbook/test_trr.py
+++ tests/cookbook/test_trr.py
@@ -6,6 +6,7 @@ from pygears.cookbook.verif import directed, verif
 from pygears.sim import sim
 from pygears.sim.modules.seqr import seqr
 from pygears.sim.modules.socket import SimSocket
+from pygears.sim.modules.verilator import SimVerilated
 from pygears.typing import Queue, Uint
 
 
@@ -19,6 +20,22 @@ def test_socket_sim():
         seqr(t=Queue[Uint[16]], seq=[list(range(9)),
                                      list(range(3))]),
         f=trr(sim_cls=SimSocket),
+        ref=[[[0, 1, 2, 3, 4, 5, 6, 7, 8], [0, 1, 2, 3, 4, 5, 6, 7, 8],
+              [0, 1, 2, 3, 4, 5, 6, 7, 8]], [[0, 1, 2], [0, 1, 2], [0, 1, 2]]])
+
+    sim()
+
+
+@with_setup(clear)
+def test_verilate_sim():
+    directed(
+        seqr(t=Queue[Uint[16]], seq=[list(range(9)),
+                                     list(range(3))]),
+        seqr(t=Queue[Uint[16]], seq=[list(range(9)),
+                                     list(range(3))]),
+        seqr(t=Queue[Uint[16]], seq=[list(range(9)),
+                                     list(range(3))]),
+        f=trr(sim_cls=SimVerilated),
         ref=[[[0, 1, 2, 3, 4, 5, 6, 7, 8], [0, 1, 2, 3, 4, 5, 6, 7, 8],
               [0, 1, 2, 3, 4, 5, 6, 7, 8]], [[0, 1, 2], [0, 1, 2], [0, 1, 2]]])
 
