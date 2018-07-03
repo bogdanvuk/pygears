88e1408293517a05a812e5b8c354c7712d21c4fb
Andrea Erdeljan <andrea.erdeljan-ext@frobas.com>
2018-07-03 13:30:13 +0200

    serialize to common

--- pygears/common/serialize.py
+++ pygears/common/serialize.py
@@ -3,5 +3,7 @@ from pygears.typing import Array
 
 
 @gear
-def serialize(din: Array['w_din', 'size']) -> b'w_din':
-    pass
+async def serialize(din: Array['w_din', 'size']) -> b'w_din':
+    async with din as val:
+        for i in range(len(val)):
+            yield val[i]
--- pygears/cookbook/serialize.py
+++
@@ -1,9 +0,0 @@
-from pygears import gear
-from pygears.typing import Array
-
-
-@gear
-async def serialize(din: Array['data_t', 'brick_size']) -> b'data_t':
-    async with din as val:
-        for i in range(len(val)):
-            yield val[i]
