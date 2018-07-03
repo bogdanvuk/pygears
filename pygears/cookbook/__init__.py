0cc11651c4f207362d8e48c7f1c87399b4e9128c
Stefan Tambur <stefan.tambur@frobas.com>
2018-07-03 18:52:47 +0200

    Add fill

--- pygears/cookbook/__init__.py
+++ pygears/cookbook/__init__.py
@@ -3,7 +3,12 @@ from .iceil import iceil
 from .priority_mux import priority_mux
 from .qcnt import qcnt
 from .sdp import sdp
+from .fill import fill
+from .chop import chop
+from .trr import trr
+from .replicate import replicate
 
 __all__ = [
-    'rng', 'iceil', 'priority_mux', 'qcnt', 'sdp'
+    'rng', 'iceil', 'priority_mux', 'qcnt', 'sdp', 'fill', 'chop', 'trr',
+    'replicate'
 ]
