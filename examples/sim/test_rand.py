160ca84a4dee54fea259808ae361fffa6c3daa05
Andrea Erdeljan <andrea.erdeljan-ext@frobas.com>
2018-07-04 17:57:37 +0200

    socket communication with cadence

--- examples/sim/test_rand.py
+++ examples/sim/test_rand.py
@@ -1,6 +1,7 @@
 from pygears.typing import Tuple, Uint, Int, Queue
-from pygears.sim.modules.svrand import create_type_cons, get_svrand_constraint
-
+from pygears.sim.modules.svrand import (create_type_cons,
+                                        get_svrand_constraint, SVRandSocket)
+import threading
 import pprint
 
 t_cfg = Tuple[{'x': Uint[3], 'y': Uint[2], 'z': Int[8]}]
@@ -19,8 +20,33 @@ cons.append(create_type_cons(t_din, 'din', cons=['din.eot == 0']))
 cons.append(
     create_type_cons(t_ain, 'ain', cons=['ain.eot != 0', 'ain.data == 10']))
 
-pp = pprint.PrettyPrinter(indent=2)
-for c in cons:
-    pp.pprint(vars(c))
+# pp = pprint.PrettyPrinter(indent=2)
+# for c in cons:
+#     pp.pprint(vars(c))
 
-get_svrand_constraint(outdir, cons)
+
+def start_cadence():
+    get_svrand_constraint(outdir, cons)
+
+
+def get_random_data():
+    soc = SVRandSocket()
+
+    for i in range(1, 3):
+        print(f'test_rand: Try to get data #{i}')
+        data = soc.get_rand(1, t_cfg)
+        print(f'test_rand: cfg got {data}')
+        data = soc.get_rand(2, t_din)
+        print(f'test_rand: din got {data}')
+        data = soc.get_rand(3, t_ain)
+        print(f'test_rand: ain got {data}')
+
+    soc.finish()
+
+
+if __name__ == '__main__':
+    tasks = [get_random_data, start_cadence]
+
+    for task in tasks:
+        t = threading.Thread(target=task)
+        t.start()
--- pygears/sim/modules/svrand.py
+++ pygears/sim/modules/svrand.py
@@ -1,7 +1,13 @@
-from pygears.util.fileio import save_file
+import math
 import os
+import socket
+
 import jinja2
+
+from pygears.definitions import ROOT_DIR
 from pygears.svgen.util import svgen_typedef
+from pygears.util.fileio import save_file
+from pygears.sim.modules.socket import u32_bytes_decode, u32_repr
 
 
 class SVRandCompileError(Exception):
@@ -37,11 +43,71 @@ def get_svrand_constraint(outdir, cons):
     env.globals.update(zip=zip, int=int, print=print, issubclass=issubclass)
 
     context = {'tcons': cons}
-
     res = env.get_template('svrand_top.j2').render(context)
     save_file('svrand_top.sv', outdir, res)
 
+    dpi_path = os.path.abspath(os.path.join(ROOT_DIR, 'sim', 'dpi'))
     ret = os.system(
-        f'irun -64bit {outdir}/svrand_top.sv -top top +svseed=random')
+        f'irun -64bit -incdir {dpi_path} {dpi_path}/sock.sv {dpi_path}/socket_pkg.sv {dpi_path}/sock.c {outdir}/svrand_top.sv -top top +svseed=random'
+    )
     if ret != 0:
         raise SVRandCompileError(f'Constrained random compilation failed.')
+
+
+class SVRandSocket:
+    SVRAND_CONN_NAME = "_svrand"
+
+    def __init__(self):
+
+        # Create a TCP/IP socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Bind the socket to the port
+        server_address = ('localhost', 1234)
+        print('starting up on %s port %s' % server_address)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.setblocking(True)
+
+        self.sock.bind(server_address)
+
+        # Listen for incoming connections
+        self.sock.listen(1)
+
+        # def setup(self):
+        print("Wait for connection")
+        conn, addr = self.sock.accept()
+
+        msg = conn.recv(1024)
+        port_name = msg.decode()
+
+        self.conn = conn
+
+        print(f"Connection received for {port_name}")
+
+    def get_rand(self, req, dtype):
+        data = None
+
+        # Send request
+        pkt = req.to_bytes(4, byteorder='little')
+        self.conn.sendall(b'\x01\x00\x00\x00' + pkt)
+
+        # Get random data
+        while data is None:
+            try:
+                buff_size = math.ceil(int(dtype) / 8)
+                if buff_size < 4:
+                    buff_size = 4
+                if buff_size % 4:
+                    buff_size += 4 - (buff_size % 4)
+                data = self.conn.recv(buff_size)
+            except socket.error:
+                print('SVRandSocket: socket error on {SVRAND_CONN_NAME}')
+        data = u32_bytes_decode(data, dtype)
+        return data
+
+    def finish(self):
+        print(f"Closing connection for _svrand")
+        self.conn.sendall(b'\x00\x00\x00\x00')
+        self.conn.close()
+        print("Closing socket server")
+        self.sock.close()
