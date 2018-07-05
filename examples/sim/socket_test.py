c0b455aef402eafa7e150586e7758b506678459c
Andrea Erdeljan <andrea.erdeljan-ext@frobas.com>
2018-07-05 16:06:06 +0200

    svrandsocket as extension; one connection for it and simsocket

--- examples/sim/socket_test.py
+++ examples/sim/socket_test.py
@@ -1,17 +1,18 @@
 from pygears.cookbook.replicate import replicate
+from pygears.cookbook.trr import trr
 from pygears.cookbook.verif import verif
+import random
 from pygears.typing import TLM
 from pygears.sim.modules.socket import SimSocket
 from pygears.sim import sim
-from pygears.typing import Tuple, Uint
-from pygears.sim import clk
-import random
+from pygears.typing import Tuple, Uint, Queue
 from pygears import GearDone, gear
-from pygears.sim.modules.svrand import (create_type_cons,
-                                        get_svrand_constraint, SVRandSocket)
+from pygears.sim.extens.svrand import (create_type_cons, SVRandSocket,
+                                       get_rand_data)
+from pygears.sim import clk
 
 t_din = Tuple[Uint[16], Uint[16]]
-outdir = '/tools/home/tmp1/cons'
+outdir = '/tools/home/tmp1'
 cons = []
 cons.append(
     create_type_cons(
@@ -22,9 +23,10 @@ cons.append(
 
 @gear
 async def get_random_data(*, t) -> TLM['t']:
-    soc = SVRandSocket(cons)
+    # soc = SVRandSocket(cons)
     for i in range(10):
-        data = soc.get_rand('din')
+        data = get_rand_data('din')
+        print(f'Got data: {data}')
 
         dly = random.randint(5, 10)
         for i in range(dly):
@@ -32,20 +34,54 @@ async def get_random_data(*, t) -> TLM['t']:
 
         yield data
 
-    soc.finish()
+    # soc.finish()
     raise GearDone
 
 
 def main():
-    get_svrand_constraint(outdir, cons, start_cadence=False)
     verif(
+        # seqr(t=t_din, seq=[(1, 2), (3, 4)]),
         get_random_data(t=t_din),
         f=replicate(sim_cls=SimSocket),
         ref=replicate,
         dly_out=True)
 
-    sim(outdir='/tools/home/tmp1')
+    sim(outdir=outdir, extens=[SVRandSocket], constraints=cons)
+
+
+t_trr = Queue[Uint[16]]
+trr_cons = []
+trr_cons.append(
+    create_type_cons(t_trr, 'din0', cons=['din0.eot dist {0:=90, 1:=10}']))
+trr_cons.append(
+    create_type_cons(t_trr, 'din1', cons=['din1.data dist {0:=50, 1:=50}']))
+trr_cons.append(
+    create_type_cons(t_trr, 'din2', cons=['din2.data dist {0:=10, 1:=90}']))
+
+
+def get_data(i):
+    x = 0
+    while x == 0:
+        data = get_rand_data(f'din{i}')
+        yield data[0]
+        x = data[1]
+
+
+@gear
+async def vir_seqr(*, t=t_trr) -> (TLM['t'], ) * 3:
+    for x in range(10):
+        yield (get_data(0), None, None)
+        yield (None, get_data(1), None)
+        yield (None, None, get_data(2))
+    raise GearDone
+
+
+def trr_main():
+    sequencers = vir_seqr()
+    verif(*sequencers, f=trr(sim_cls=SimSocket), ref=trr(name='ref_model'))
+
+    sim(outdir=outdir, extens=[SVRandSocket], constraints=trr_cons)
 
 
 if __name__ == '__main__':
-    main()
+    trr_main()
---
+++ pygears/sim/extens/svrand.py
@@ -0,0 +1,165 @@
+import math
+import os
+import socket
+
+import jinja2
+
+from pygears import registry
+from pygears.sim.modules.socket import u32_bytes_decode
+from pygears.svgen.util import svgen_typedef
+from pygears.util.fileio import save_file
+
+from pygears.sim.modules.socket import SimSocket
+
+
+def get_rand_data(name):
+    # print(f'Getting random data for {name}')
+    svrand = registry('SimConfig')['SVRandSocket']
+    req, dtype = svrand.parse_name(name)
+    simsoc = registry('SimConfig')['SimSocket']
+    data = simsoc.send_req(req, dtype)
+    return data
+
+
+class SVRandError(Exception):
+    pass
+
+
+class SVRandConstraints:
+    def __init__(self, name='dflt', cons=[], dtype=None):
+        self.name = name
+        self.cons = cons.copy()
+        self.dtype = dtype
+        self.cvars = {}
+        self.cvars[name] = svgen_typedef(dtype, name)
+
+    def add_var(self, name, dtype):
+        self.cvars[name] = svgen_typedef(dtype, name)
+
+
+def create_type_cons(dtype, name, cons, **var):
+    tcons = SVRandConstraints(name, cons, dtype)
+    for name, dtype in var.items():
+        tcons.add_var(name, dtype)
+
+    return tcons
+
+
+class SVRandSocket:
+    SVRAND_CONN_NAME = "_svrand"
+
+    def __init__(self, top, conf):
+        self.outdir = conf['outdir']
+
+        try:
+            self.constraints = conf['constraints']
+        except KeyError:
+            raise SVRandError(f'No constraints passed to init')
+
+        if 'rand_port' in conf:
+            self.port = conf['rand_port']
+        else:
+            self.port = 4567
+
+        self.open_sock = True
+        sim = registry('Simulator')
+        sim.events['before_setup'].append(self.before_setup)
+        sim.events['after_run'].append(self.after_run)
+        registry('SimConfig')['SVRandSocket'] = self
+
+    def before_setup(self, sim):
+        sim_map = registry('SimMap')
+        for module, sim_gear in sim_map.items():
+            if isinstance(sim_gear, SimSocket):
+                self.open_sock = False
+
+        if self.open_sock:
+            self.connect()
+        else:
+            hooks = {}
+            # hooks['includes'] = ['svrand_top.sv']
+            hooks['module_init'] = 'svrand_top rand_i();'
+            hooks[
+                'synchro_req'] = 'if (data != 0) ret = rand_i.get_rand(synchro_handle, data);'
+            registry('SimConfig')['SimSocketHooks'] = hooks
+            self.create_svrand_top()
+
+    def connect(self):
+        # Create a TCP/IP socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Bind the socket to the port
+        server_address = ('localhost', self.port)
+        print('starting up on %s port %s' % server_address)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.setblocking(True)
+
+        self.sock.bind(server_address)
+
+        # Listen for incoming connections
+        self.sock.listen(1)
+
+        print("Wait for connection")
+        conn, addr = self.sock.accept()
+
+        msg = conn.recv(1024)
+        port_name = msg.decode()
+
+        self.conn = conn
+
+        print(f"Connection received for {port_name}")
+
+    def parse_name(self, name):
+        for i, c in enumerate(self.constraints):
+            if c.name == name:
+                dtype = c.dtype
+                req = i + 1
+                break
+        return req, dtype
+
+    def send_req(self, req, dtype):
+        data = None
+
+        # Send request
+        pkt = req.to_bytes(4, byteorder='little')
+        self.conn.sendall(b'\x01\x00\x00\x00' + pkt)
+
+        # Get random data
+        while data is None:
+            try:
+                buff_size = math.ceil(int(dtype) / 8)
+                if buff_size < 4:
+                    buff_size = 4
+                if buff_size % 4:
+                    buff_size += 4 - (buff_size % 4)
+                data = self.conn.recv(buff_size)
+            except socket.error:
+                print('SVRandSocket: socket error on {SVRAND_CONN_NAME}')
+        data = u32_bytes_decode(data, dtype)
+        return data
+
+    def get_rand(self, name):
+        req, dtype = self.parse_name(name)
+        data = self.send_req(req, dtype)
+        return data
+
+    def after_run(self, sim):
+        if self.open_sock:
+            print(f"Closing connection for _svrand")
+            self.conn.sendall(b'\x00\x00\x00\x00')
+            self.conn.close()
+            print("Closing socket server")
+            self.sock.close()
+
+    def create_svrand_top(self):
+        base_addr = os.path.dirname(__file__)
+        env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(base_addr),
+            trim_blocks=True,
+            lstrip_blocks=True)
+        env.globals.update(
+            zip=zip, int=int, print=print, issubclass=issubclass)
+
+        context = {'tcons': self.constraints, 'open_sock': self.open_sock}
+        res = env.get_template('svrand_top.j2').render(context)
+        save_file('svrand_top.sv', self.outdir, res)
