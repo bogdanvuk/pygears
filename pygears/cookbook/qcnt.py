aec660e9f3b948c0aca0e52d612759183bef25dc
Andrea Erdeljan <andrea.erdeljan-ext@frobas.com>
2018-07-09 20:29:41 +0200

    qcnt ref model; qcnt and trr_dist tests

--- pygears/cookbook/qcnt.py
+++ pygears/cookbook/qcnt.py
@@ -5,8 +5,18 @@ from pygears.typing import Queue, Uint
 
 
 @gear
-def qcnt(din: Queue, *, lvl=1, w_out=16) -> Queue[Uint['w_out']]:
-    pass
+async def qcnt(din: Queue, *, lvl=1, w_out=16) -> Queue[Uint['w_out']]:
+    val = (0, ) * din.dtype.lvl
+    cnt = 0
+    last_el = False
+    while not last_el:
+        async with din as val:
+            last_el = all(v for v in val[1:])
+            cnt_mode = True if (lvl == din.dtype.lvl) else all(
+                v for v in val[1:din.dtype.lvl - lvl + 1])
+            if cnt_mode:
+                yield (cnt, last_el)
+                cnt += 1
 
 
 class SVGenQcnt(SVModuleGen):
