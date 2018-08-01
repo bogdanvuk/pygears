a8ac78c7cc622d51c8af5a24e015918a0059c723
Bogdan Vukobratovic <bogdan.vukobratovic@kortiq.com>
2018-08-01 12:55:24 +0200

    Fix filt, add union_collapse

--- pygears/common/cast.py
+++ pygears/common/cast.py
@@ -2,7 +2,7 @@ from pygears import module, gear
 from pygears.typing.base import TypingMeta, typeof
 from pygears.core.intf import IntfOperPlugin
 from pygears.typing_common.codec import code, decode
-from pygears.typing import Int, Tuple, Queue, Uint
+from pygears.typing import Int, Tuple, Queue, Uint, Union
 
 
 @gear
@@ -18,6 +18,9 @@ async def cast(din, *, cast_type) -> b'cast(din, cast_type)':
         elif typeof(cast_type, Tuple) and typeof(
                 din.dtype, Queue) and not cast_type.is_specified():
             dout = module().tout((d[0], d[1:]))
+        elif (typeof(cast_type, Union) and typeof(din.dtype, Tuple)
+              and len(din.dtype) == 2 and not cast_type.is_specified()):
+            pass
         else:
             dout = decode(module().tout, code(din.dtype, d))
 
