760d475dc58a27f8f34e67a2e7f434cb659c994e
Andrea Erdeljan <andrea.erdeljan-ext@frobas.com>
2018-08-08 11:33:14 +0200

    serialize: parameter fixed to be universal for all simulators

--- svlib/serialize.sv
+++ svlib/serialize.sv
@@ -1,31 +1,33 @@
-module serialize (
-                  input clk,
-                  input rst,
-                  dti.consumer din,
-                  dti.producer dout
-                  );
+module serialize # (
+                    parameter DIN = 0,
+                    parameter DOUT = 0
+                    )
+   (
+    input clk,
+    input rst,
+    dti.consumer din,
+    dti.producer dout
+    );
 
    // ---------------------------------------------------------------------------
    // internal signals
    // ---------------------------------------------------------------------------
 
-   localparam W_DIN = din.W_DATA;
-   localparam W_DOUT = dout.W_DATA;
-   localparam DIN_SIZE = W_DIN / W_DOUT;
+   localparam DIN_SIZE = DIN / DOUT;
 
    logic [$clog2(DIN_SIZE)-1 : 0] count_s;
-   logic [W_DOUT-1 : 0] splitted_input[DIN_SIZE-1 : 0];
+   logic [DOUT-1 : 0] splitted_input[DIN_SIZE-1 : 0];
 
    logic last_data_s;
-   assign last_data_s = (count_s == ($size(din.data)/W_DOUT-1));
+   assign last_data_s = (count_s == (DIN/DOUT-1));
 
    // ---------------------------------------------------------------------------
    // interfaces
    // ---------------------------------------------------------------------------
 
    generate
-      for (genvar i = 0; i < $size(din.data)/W_DOUT; i++) begin
-         assign splitted_input[i] = din.data[((i+1)*W_DOUT)-1 : (i*W_DOUT)];
+      for (genvar i = 0; i < DIN/DOUT; i++) begin
+         assign splitted_input[i] = din.data[((i+1)*DOUT)-1 : (i*DOUT)];
       end
    endgenerate
 
