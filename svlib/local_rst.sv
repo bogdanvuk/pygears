e78cd0eab9b5ac492e2006895bd0e84ce844418a
Andrea Erdeljan <andrea.erdeljan-ext@frobas.com>
2018-08-13 10:02:17 +0200

    syntax fix

--- svlib/local_rst.sv
+++ svlib/local_rst.sv
@@ -3,10 +3,10 @@ module local_rst
    input  clk,
    input  rst,
    output local_rst,
-   dti.consumer din,
+   dti.consumer din
    );
 
    assign din.ready = 1;
-   assign local_rst = din.valid || rst
+   assign local_rst = din.valid || rst;
 
 endmodule
