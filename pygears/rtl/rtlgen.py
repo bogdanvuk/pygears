4393303a8ea64754745c82923575340ed570d674
Bogdan Vukobratovic <bogdan.vukobratovic@kortiq.com>
2018-08-15 16:20:44 +0200

    Move basename and outname deduction to RTLIntf

---
+++ pygears/rtl/rtlgen.py
@@ -0,0 +1,24 @@
+from pygears.registry import PluginBase
+from pygears import registry, find
+from pygears.rtl.inst import rtl_inst
+from pygears.rtl.connect import rtl_connect
+from pygears.rtl.channel import RTLChannelVisitor
+
+
+def rtlgen(top=None, **conf):
+
+    if top is None:
+        top = registry('HierRoot')
+    elif isinstance(top, str):
+        top = find(top)
+
+    for oper in registry('RTLFlow'):
+        top = oper(top, conf)
+
+    return top
+
+
+class RTLPlugin(PluginBase):
+    @classmethod
+    def bind(cls):
+        cls.registry['RTLFlow'] = [rtl_inst, rtl_connect, RTLChannelVisitor]
