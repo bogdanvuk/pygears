56008bbdc2ee63f8ca7b5d01a1efece313147752
Andrea Erdeljan <andrea.erdeljan-ext@frobas.com>
2018-09-26 12:15:58 +0200

    log wrap for monkeypatching logger functions

--- pygears/core/log.py
+++ pygears/core/log.py
@@ -1,7 +1,10 @@
 from pygears.registry import PluginBase
-from functools import partial
+from pygears import registry, bind
 import sys
 import logging
+from string import Template
+
+registry_log_name = Template('${name}Log')
 
 
 def core_log():
@@ -12,33 +15,68 @@ def typing_log():
     return logging.getLogger('typing')
 
 
-def warning_to_exception(message, name, *args, **kws):
-    log = logging.getLogger(name)
-    if log.isEnabledFor(logging.WARNING):
-        log._log(logging.WARNING, message, args, **kws)
-        raise Exception(message)
+class LogWrap:
+    def __init__(self, logger):
+        self.logger = logger
+        self.name = logger.name
+
+    def severity_action(self, severity, message):
+        log_cfg = registry(registry_log_name.substitute(name=self.name))
+        if log_cfg[severity]['exception']:
+            raise Exception(message)
+        elif log_cfg[severity]['debug']:
+            import pdb
+            pdb.set_trace()
+
+    def warning(self, message, *args, **kws):
+        if self.logger.isEnabledFor(logging.WARNING):
+            self.logger._log(logging.WARNING, message, args, **kws)
+            self.severity_action('warning', message)
+
+    def error(self, message, *args, **kws):
+        if self.logger.isEnabledFor(logging.ERROR):
+            self.logger._log(logging.ERROR, message, args, **kws)
+            self.severity_action('error', message)
 
 
-def get_default_logger_handler(verbosity):
-    fmt = logging.Formatter('%(name)s %(module)s [%(levelname)s]: %(message)s')
-    ch = logging.StreamHandler(sys.stdout)
-    ch.setLevel(verbosity)
-    ch.setFormatter(fmt)
-    return ch
+class CustomLog:
+    dflt_action = {'debug': False, 'exception': False}
+    dflt_severity = {
+        'warning': dflt_action.copy(),
+        'error': dflt_action.copy()
+    }
 
+    def __init__(self, name, verbosity=logging.INFO):
+        self.name = name
+        self.verbosity = verbosity
 
-def set_default_logger(name, verbosity):
-    logger = logging.getLogger(name)
-    logger.setLevel(verbosity)
-    ch = get_default_logger_handler(verbosity=verbosity)
-    logger.addHandler(ch)
+        self.set_default_logger()
+        bind(
+            registry_log_name.substitute(name=name), self.dflt_severity.copy())
+
+        self.logger = logging.getLogger(name)
+
+        self.wrap = LogWrap(self.logger)
+        self.logger.warning = self.wrap.warning
+        self.logger.error = self.wrap.error
+
+    def get_default_logger_handler(self):
+        fmt = logging.Formatter(
+            '%(name)s %(module)s [%(levelname)s]: %(message)s')
+        ch = logging.StreamHandler(sys.stdout)
+        ch.setLevel(self.verbosity)
+        ch.setFormatter(fmt)
+        return ch
+
+    def set_default_logger(self):
+        logger = logging.getLogger(self.name)
+        logger.setLevel(self.verbosity)
+        ch = self.get_default_logger_handler()
+        logger.addHandler(ch)
 
 
 class LogPlugin(PluginBase):
     @classmethod
     def bind(cls):
-        set_default_logger('core', logging.INFO)
-        set_default_logger('typing', logging.INFO)
-
-        # core_log().warning = partial(warning_to_exception, name='core')
-        # typing_log().warning = partial(warning_to_exception, name='typing')
+        CustomLog('core')
+        CustomLog('typing')
