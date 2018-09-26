573ca45559759ab9bd46e4b452391d67dbe15752
drakanovic <damjan.rakanovic-ext@frobas.com>
2018-09-26 17:25:33 +0200

    Modify width reductor

--- pygears/cookbook/width_reductor.py
+++ pygears/cookbook/width_reductor.py
@@ -1,10 +1,14 @@
 from pygears import gear
-from pygears.typing import Uint, Queue, Array
+from pygears.typing import Uint, Queue, Array, Tuple
 
-
-TDin = Array[Uint['w_data'],'no']
+TDin = Tuple[Array[Uint['w_data'], 'no'], Uint['w_active']]
 TOut = Queue[Uint['w_data']]
 
+
 @gear(outnames=['dout'])
-def width_reductor(din: TDin) -> TOut:
+def width_reductor(din: TDin,
+                   *,
+                   w_data=b'w_data',
+                   no=b'no',
+                   w_active=b'w_active') -> TOut:
     pass
