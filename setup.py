ea15c949cf92dfcebd5fd9ab75a26d6cd3a05dbe
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2018-10-04 17:20:47 +0200

    Turn sim/extens into package

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ def readme():
 
 setup(
     name='pygears',
-    version='0.1.1a',
+    version='0.1.1a2',
     description='Framework for functional hardware design approach',
     long_description=readme(),
 
