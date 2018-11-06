c0e6e402bc8e924aeedcc63f5891366fcf468e6f
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2018-11-06 23:20:19 +0100

    Move some more modules j2 -> svt

--- pygears/cookbook/chunk_concat.py
+++ pygears/cookbook/chunk_concat.py
@@ -1,6 +1,4 @@
 from pygears import gear
-from pygears.svgen.inst import SVGenInstPlugin
-from pygears.svgen.svmod import SVModuleGen
 from pygears.typing import Queue, Uint, Tuple
 
 
@@ -18,26 +16,3 @@ def chunk_concat(cfg: Uint['Tn'],
                  chunk_size=1,
                  pad=0) -> b'chunk_type(din, chunk_size)':
     pass
-
-
-class SVGenChunkConcat(SVModuleGen):
-    @property
-    def is_generated(self):
-        return True
-
-    def get_module(self, template_env):
-        context = {
-            'module_name': self.sv_module_name,
-            'intfs': list(self.sv_port_configs()),
-            'cnt_type': self.node.params['cnt_type'],
-            'chunk_size': self.node.params['chunk_size'],
-            'pad': self.node.params['pad']
-        }
-        return template_env.render_local(__file__, "chunk_concat.j2", context)
-
-
-class SVGenChunkConcatPlugin(SVGenInstPlugin):
-    @classmethod
-    def bind(cls):
-        cls.registry['svgen']['module_namespace'][
-            chunk_concat] = SVGenChunkConcat
