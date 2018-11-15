6857f8e39c9253e0684d640c47c71399c0ad6350
andrea-e <andrea.erdeljan@gmail.com>
2018-11-15 16:11:07 +0100

    added auto_bin_max, initial coverage merge

--- pygears/sim/extens/coverage.py
+++ pygears/sim/extens/coverage.py
@@ -2,6 +2,9 @@ import collections
 import functools
 import inspect
 import itertools
+import pickle
+import time
+import os
 from enum import IntEnum
 
 from pygears.typing.visitor import TypingVisitorBase
@@ -43,11 +46,13 @@ class CoverPoint:
                  bins=None,
                  threshold=None,
                  bind_field_name=None,
-                 bind_dtype=False):
+                 bind_dtype=False,
+                 auto_bin_max=64):
         self.name = name
         self.dtype = dtype
         self.bind_field_name = bind_field_name
         self.bind_dtype = bind_dtype
+        self.auto_bin_max = auto_bin_max
         if bins:
             self.bins = bins
             self.set_default_bin()
@@ -79,11 +84,20 @@ class CoverPoint:
     def set_auto_bin(self):
         assert self.dtype, f'dtype must be set for CoverPoint if bins set as None'
         bins = []
-        for i in range(2**len(self.dtype)):
+        if self.auto_bin_max < 2**len(self.dtype):
+            bin_num = self.auto_bin_max
+            bin_rng = 2**len(self.dtype) // self.auto_bin_max
+        else:
+            bin_num = 2**len(self.dtype)
+            bin_rng = 1
+
+        for i in range(bin_num):
             bins.append(
                 CoverBin(
                     f'auto_bin{i}',
-                    enablement=(lambda y: (lambda x: (y == x)))(i)))
+                    enablement=
+                    (lambda y: (lambda x: (x in range(y * bin_rng, (y + 1) * bin_rng)))
+                     )(i)))
         return bins
 
     def sample(self, val):
@@ -140,12 +154,6 @@ class CoverageTypeVisitor(TypingVisitorBase):
     def visit_uint(self, dtype, field=None, data=None):
         self.sample(dtype, field, data)
 
-    def report(self):
-        r = ''
-        for p in self.cover_points:
-            r += f'{p.report()}\n'
-        return r
-
 
 class CoverGroup:
     '''Group holds all cover points for one variable
@@ -154,6 +162,7 @@ class CoverGroup:
     def __init__(self, name, dtype, cover_points=None):
         self.name = name
         self.dtype = dtype
+        self.cover_points = cover_points
         self.visitor = CoverageTypeVisitor(
             'coverage_visitor', cover_points=cover_points)
 
@@ -164,12 +173,14 @@ class CoverGroup:
         report = '-' * 80
         report += f'\nCoverge report for group {self.name}\n'
         report += '-' * 80
-        report += f'\n{self.visitor.report()}'
+        report += '\n'
+        for p in self.cover_points:
+            report += f'{p.report()}\n'
         report += '-' * 80
         return report
 
 
-class CoverSingleValue():
+class CoverSingleValue:
     '''Coverage of single unpacked values. Combine before sampling'''
 
     def __init__(self, cg=None, en=True):
@@ -280,3 +291,36 @@ def cover_intf(intf, cg, en=True):
 
     c = CoverIntf(intf, cg=cg, en=en)
     intf.events['put'].append(c.event)
+
+
+def save_coverage(cg, outdir):
+    timestr = time.strftime('%Y%m%d_%H%M%S')
+    del cg.dtype
+    for i in range(len(cg.cover_points)):
+        del cg.cover_points[i].dtype
+        for j in range(len(cg.cover_points[i].bins)):
+            del cg.cover_points[i].bins[j].enablement
+    path = os.path.join(outdir, f'{cg.name}_{timestr}.p')
+    pickle.dump(cg, open(path, 'wb'))
+
+
+def merge_cover_groups(cg0, cg1):
+    cg = cg0
+    for cp_idx, cp in enumerate(cg.cover_points):
+        for bin_idx in range(len(cp.bins)):
+            cg.cover_points[cp_idx].bins[
+                bin_idx].cover_cnt += cg1.cover_points[cp_idx].bins[
+                    bin_idx].cover_cnt
+    return cg
+
+
+def merge_coverage(name, outdir):
+    merged_cg = None
+    for fn in os.listdir(outdir):
+        if name in fn:
+            curr_cg = pickle.load(open(os.path.join(outdir, fn), 'rb'))
+            if merged_cg:
+                merged_cg = merge_cover_groups(merged_cg, curr_cg)
+            else:
+                merged_cg = curr_cg
+    return merged_cg
