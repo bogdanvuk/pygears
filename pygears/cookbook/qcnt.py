88b02e127e156ee70d1bf28a0b692a190f549229
drakanovic <damjan.rakanovic-ext@frobas.com>
2018-11-21 17:17:36 +0100

    qcnt init param added

--- pygears/cookbook/qcnt.py
+++ pygears/cookbook/qcnt.py
@@ -3,9 +3,9 @@ from pygears.typing import Queue, Uint
 
 
 @gear(svgen={'compile': True})
-async def qcnt(din: Queue, *, lvl=1, w_out=16) -> Queue[Uint['w_out']]:
-    cnt = Uint[w_out](0)
+async def qcnt(din: Queue, *, lvl=1, init=1, w_out=16) -> Queue[Uint['w_out']]:
+    cnt = Uint[w_out](init)
     async for (data, eot) in din:
         if all(eot[:din.dtype.lvl - lvl]):
-            cnt += 1
             yield (cnt, all(eot))
+            cnt += 1
