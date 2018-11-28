5262bf3e69486f0dca26832e84b8d80c410f0df8
andrea-e <andrea.erdeljan@gmail.com>
2018-11-28 14:06:50 +0100

    accumulator is compiled

--- pygears/cookbook/accumulator.py
+++ pygears/cookbook/accumulator.py
@@ -2,16 +2,18 @@ from pygears import gear
 from pygears.typing import Queue, Tuple, Integer
 
 
-@gear
+@gear(svgen={'compile': True})
 async def accumulator(din: Queue[Tuple[Integer['w_data'], Integer['w_data']]]
                       ) -> b'din[0][0]':
 
-    val = din.dtype(((0, 0), 0))
-    acc = 0
+    acc = din.dtype[0][0](0)
+    offset_added = False
 
-    while not val.eot:
-        async with din as val:
-            data, offset = val.data
-            acc += int(data)
+    async for ((data, offset), eot) in din:
+        if offset_added:
+            acc = acc + int(data)
+        else:
+            acc = offset + int(data)
+            offset_added = True
 
-    yield acc + offset
+    yield acc
