b5dac7fc5e25fea956ce930d0b5e96f9050ba10c
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2018-12-10 23:24:07 +0100

    Refactor activity monitor

--- pygears/sim/extens/activity.py
+++ pygears/sim/extens/activity.py
@@ -1,4 +1,4 @@
-from pygears import registry
+from pygears import bind
 from pygears.common.decoupler import decoupler_din
 from pygears.common import const
 from pygears.sim import sim_log
@@ -10,6 +10,7 @@ class ActivityChecker:
     def __init__(self, top, sim=Inject('sim/simulator')):
         sim.events['before_run'].append(self.before_run)
         sim.events['after_run'].append(self.after_run)
+        bind('sim/activity', self)
         self.blockers = {}
         self.hooks = {}
 
@@ -31,6 +32,17 @@ class ActivityChecker:
                 p.consumer.events['pull_start'].append(self.intf_pull_start)
                 p.consumer.events['pull_done'].append(self.intf_pull_done)
 
+    def get_port_status(self, port):
+        print("Get port status")
+        q = port.get_queue()
+        if q._unfinished_tasks:
+            return "active"
+
+        if port in self.blockers:
+            return "waited"
+
+        return "empty"
+
     def after_run(self, sim):
         for sim_gear in sim.sim_gears:
             module = sim_gear.gear
@@ -42,9 +54,10 @@ class ActivityChecker:
                     sim_log().error(f'Data left in decoupler: {module.name}')
 
             for p in module.in_ports:
-                q = p.get_queue()
-                if q._unfinished_tasks:
-                    src_port = q.intf.consumers[0]
+                status = self.get_port_status(p)
+
+                if status == "active":
+                    src_port = p.get_queue().intf.consumers[0]
 
                     if src_port.gear.definition == const:
                         # Skip constants since they are never done
@@ -56,7 +69,7 @@ class ActivityChecker:
                         f'{src_port.gear.name}.{src_port.basename} -> {module.name}.{p.basename} was not acknowledged'
                     )
 
-                if p in self.blockers:
+                if status == "waited":
                     src_port = self.blockers[p]
                     if 'waiting' in self.hooks:
                         self.hooks['waiting'](module, p)
