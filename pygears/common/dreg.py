62f3102bafbc6548123b154e574809120df635e2
Bogdan Vukobratovic <bogdan.vukobratovic@kortiq.com>
2019-03-14 16:52:45 +0100

    Fix initial reset in verilator

--- pygears/common/dreg.py
+++ pygears/common/dreg.py
@@ -1,42 +1,21 @@
-from pygears import gear, QueueEmpty, module
-from pygears.sim import clk
+from pygears import gear, QueueEmpty
+from pygears.typing import Bool
 
 
-def setup(module):
-    module.reg = None
+@gear(svgen={'compile': True})
+async def dreg(din) -> b'din':
+    data = din.dtype.decode(0)
+    valid = Bool(False)
 
+    while True:
+        if valid:
+            yield data
 
-@gear(sim_setup=setup, svgen={'svmod_fn': 'dreg.sv'})
-async def dreg(din: 'tdin') -> b'tdin':
-    if module().reg is None:
-        module().reg = await din.get()
-    else:
-
-        yield module().reg
-
-        try:
-            module().reg = din.get_nb()
-        except QueueEmpty:
-            module().reg = None
-
-# from pygears import gear, QueueEmpty
-# from pygears.typing import Bool
-
-
-# @gear(svgen={'compile': True})
-# async def dreg(din: 'tdin') -> b'tdin':
-#     data = din.dtype.decode(0)
-#     valid = Bool(False)
-
-#     while True:
-#         if valid:
-#             yield data
-
-#             try:
-#                 data = din.get_nb()
-#                 valid = True
-#             except QueueEmpty:
-#                 valid = False
-#         else:
-#             data = await din.get()
-#             valid = True
+            try:
+                data = din.get_nb()
+                valid = True
+            except QueueEmpty:
+                valid = False
+        else:
+            data = await din.get()
+            valid = True
