76c120e2d090e57f92f6e1efc7cb936e4c3dd2b0
andrea-e <andrea.erdeljan@gmail.com>
2019-04-12 21:09:29 +0200

    hier. cleanup of condition expressions

--- pygears/hls/cleanup.py
+++ pygears/hls/cleanup.py
@@ -2,13 +2,13 @@ from .conditions import cond_name_match
 from .inst_visit import InstanceVisitor
 
 
-class ConditionCleanup(InstanceVisitor):
+class ConditionExprCleanup(InstanceVisitor):
     def __init__(self, conds):
         self.conditions = conds
 
-    def same_cond(self, find_cond):
+    def visit_str(self, node):
         for cond in self.conditions:
-            if cond.target == find_cond:
+            if cond.target == node:
                 if cond_name_match(cond.val):
                     new_cond = cond.val
                     if new_cond is None:
@@ -16,15 +16,45 @@ class ConditionCleanup(InstanceVisitor):
 
                     while new_cond is not None:
                         last_cond = new_cond
-                        new_cond = self.same_cond(last_cond)
+                        new_cond = self.visit(last_cond)
 
                     return last_cond
 
         return None
 
+    def generic_visit(self, node):
+        pass
+
+    def visit_UnaryOpExpr(self, node):
+        operand = self.visit(node.operand)
+        if operand is not None:
+            breakpoint()
+            node.operand = operand
+            return node
+        return None
+
+    def visit_BinOpExpr(self, node):
+        operands = []
+        for op in node.operands:
+            operands.append(self.visit(op))
+
+        if any(operands):
+            for i in range(len(operands)):
+                if operands[i] is None:
+                    operands[i] = node.operands[i]
+            node.operands = tuple(operands)
+            return node
+
+        return None
+
+
+class ConditionCleanup(InstanceVisitor):
+    def __init__(self, conds):
+        self.clean_expr = ConditionExprCleanup(conds).visit
+
     def enter_block(self, node):
         if getattr(node, 'in_cond', None):
-            new_cond = self.same_cond(node.in_cond)
+            new_cond = self.clean_expr(node.in_cond)
             if new_cond is not None:
                 node.in_cond = new_cond
 
@@ -35,7 +65,7 @@ class ConditionCleanup(InstanceVisitor):
         if stmt.width:
             return
 
-        cond = self.same_cond(stmt.val)
+        cond = self.clean_expr(stmt.val)
         if cond is not None:
             stmt.val = cond
 
@@ -68,5 +98,11 @@ def condition_cleanup(hdl_stmts):
 
     for _, val in hdl_stmts.items():
         ConditionCleanup(conditions).visit(val)
+
+    if conditions:
+        hdl_stmts['conditions'].stmts = [
+            c for c in conditions
+            if not cond_name_match(c.val) or c.target == 'rst_cond'
+        ]
 
     return hdl_stmts
