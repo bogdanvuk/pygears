3543ac4ad2faa0bea6bf7ddec8ac46aaf6c9479a
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2019-05-27 18:55:00 +0200

    zip_sync -> svt

--- tests/common/test_czip.py
+++ tests/common/test_czip.py
@@ -1,7 +1,7 @@
 import pytest
 from pygears import Intf
 from pygears.typing import Queue, Tuple, Uint
-from pygears.common import czip
+from pygears.common import czip, zip_sync
 
 from pygears.sim import sim
 from pygears.cookbook.delay import delay_rng
@@ -17,7 +17,7 @@ def test_general():
 
 @pytest.mark.parametrize('din_delay', [0, 5])
 @pytest.mark.parametrize('dout_delay', [0, 5])
-def test_cosim(tmpdir, cosim_cls, din_delay, dout_delay):
+def test_cosim_one_queue(tmpdir, cosim_cls, din_delay, dout_delay):
     verif(drv(t=Uint[8], seq=list(range(10)))
           | delay_rng(din_delay, din_delay),
           drv(t=Queue[Uint[8]], seq=[list(range(10))])
@@ -25,5 +25,59 @@ def test_cosim(tmpdir, cosim_cls, din_delay, dout_delay):
           f=czip(sim_cls=cosim_cls),
           ref=czip(name='ref_model'),
           delays=[delay_rng(dout_delay, dout_delay)])
+
+    sim(outdir=tmpdir)
+
+
+@pytest.mark.parametrize('din_delay', [0, 5])
+@pytest.mark.parametrize('dout_delay', [0, 5])
+def test_cosim_both_queue(tmpdir, cosim_cls, din_delay, dout_delay):
+    verif(drv(t=Queue[Uint[8]], seq=[list(range(10))])
+          | delay_rng(din_delay, din_delay),
+          drv(t=Queue[Uint[8]], seq=[list(range(10))])
+          | delay_rng(din_delay, din_delay),
+          f=czip(sim_cls=cosim_cls),
+          ref=czip(name='ref_model'),
+          delays=[delay_rng(dout_delay, dout_delay)])
+
+    sim(outdir=tmpdir)
+
+
+@pytest.mark.parametrize('din0_delay', [0, 1])
+@pytest.mark.parametrize('din1_delay', [0, 1])
+@pytest.mark.parametrize('dout0_delay', [0, 1])
+@pytest.mark.parametrize('dout1_delay', [0, 1])
+def test_cosim_zipsync_one_queue(tmpdir, cosim_cls, din0_delay, din1_delay,
+                                 dout0_delay, dout1_delay):
+    verif(drv(t=Uint[8], seq=list(range(10)))
+          | delay_rng(din0_delay, din0_delay),
+          drv(t=Queue[Uint[8]], seq=[list(range(10))])
+          | delay_rng(din1_delay, din1_delay),
+          f=zip_sync(sim_cls=cosim_cls),
+          ref=zip_sync(name='ref_model'),
+          delays=[
+              delay_rng(dout0_delay, dout0_delay),
+              delay_rng(dout1_delay, dout1_delay)
+          ])
+
+    sim(outdir=tmpdir)
+
+
+@pytest.mark.parametrize('din0_delay', [0, 1])
+@pytest.mark.parametrize('din1_delay', [0, 1])
+@pytest.mark.parametrize('dout0_delay', [0, 1])
+@pytest.mark.parametrize('dout1_delay', [0, 1])
+def test_cosim_zipsync_both_queue(tmpdir, cosim_cls, din0_delay, din1_delay,
+                                  dout0_delay, dout1_delay):
+    verif(drv(t=Queue[Uint[8], 2], seq=[[list(range(10)) for _ in range(2)]])
+          | delay_rng(din0_delay, din0_delay),
+          drv(t=Queue[Uint[8], 2], seq=[[list(range(10)) for _ in range(2)]])
+          | delay_rng(din1_delay, din1_delay),
+          f=zip_sync(sim_cls=cosim_cls),
+          ref=zip_sync(name='ref_model'),
+          delays=[
+              delay_rng(dout0_delay, dout0_delay),
+              delay_rng(dout1_delay, dout1_delay)
+          ])
 
     sim(outdir=tmpdir)
