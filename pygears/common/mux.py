38e9d0642a699f1132acf5d8dca5d6150395dcb4
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2019-06-20 13:42:40 +0200

    Implement mapping in mux (missing HDL implementation)

--- pygears/common/mux.py
+++ pygears/common/mux.py
@@ -4,20 +4,42 @@ from pygears.typing import Queue, Union, typeof
 from .union import union_collapse
 
 
-def mux_type(dtypes):
-    return Union[dtypes]
+def mux_type(dtypes, mapping):
+    full_dtypes = tuple(dtypes[mapping[i]] for i in sorted(mapping))
+    return Union[full_dtypes]
+
+
+def full_mapping(dtypes, mapping):
+    dout_num = max(mapping.values()) + 1
+    fm = mapping.copy()
+
+    for i in range(len(dtypes)):
+        if i not in mapping:
+            fm[i] = dout_num
+
+    return fm
+
+
+def dflt_map(dtypes):
+    return {i: i for i in range(len(dtypes))}
 
 
 # @gear(svgen={'compile': True, 'inline_conditions': True})
 @gear
-async def mux(ctrl, *din) -> b'mux_type(din)':
+async def mux(ctrl,
+              *din,
+              mapping=b'dflt_map(din)',
+              _full_mapping=b'full_mapping(din, mapping)'
+              ) -> b'mux_type(din, _full_mapping)':
     async with ctrl as c:
-        assert c < len(din), 'mux: incorrect selection value'
+        c_map = _full_mapping[c]
+
+        assert c_map < len(din), 'mux: incorrect selection value'
         if typeof(din[0].dtype, Queue):
-            async for d in din[c]:
+            async for d in din[c_map]:
                 yield (d, c)
         else:
-            async with din[c] as d:
+            async with din[c_map] as d:
                 yield (d, c)
 
 
--- tests/common/test_mux.py
+++ tests/common/test_mux.py
@@ -5,7 +5,7 @@ from pygears.common import decoupler, mux
 from pygears.cookbook.delay import delay_rng
 from pygears.cookbook.verif import directed, drv
 from pygears.sim import sim
-from pygears.typing import Int, Queue, Uint
+from pygears.typing import Int, Queue, Uint, bitw
 from pygears.util.test_utils import formal_check
 
 
@@ -27,45 +27,61 @@ def test_uint_directed(tmpdir, sim_cls, din_delay, cfg_delay, dout_delay):
     t_din = Uint[8]
 
     dut = get_dut(dout_delay)
-    directed(
-        drv(t=t_ctrl, seq=[0, 1, 2])
-        | delay_rng(cfg_delay, cfg_delay),
-        drv(t=t_din, seq=[5])
-        | delay_rng(din_delay, din_delay),
-        drv(t=t_din, seq=[6])
-        | delay_rng(din_delay, din_delay),
-        drv(t=t_din, seq=[7])
-        | delay_rng(din_delay, din_delay),
-        f=dut(sim_cls=sim_cls),
-        ref=[(5, 0), (6, 1), (7, 2)],
-        delays=[delay_rng(dout_delay, dout_delay)])
+    directed(drv(t=t_ctrl, seq=[0, 1, 2])
+             | delay_rng(cfg_delay, cfg_delay),
+             drv(t=t_din, seq=[5])
+             | delay_rng(din_delay, din_delay),
+             drv(t=t_din, seq=[6])
+             | delay_rng(din_delay, din_delay),
+             drv(t=t_din, seq=[7])
+             | delay_rng(din_delay, din_delay),
+             f=dut(sim_cls=sim_cls),
+             ref=[(5, 0), (6, 1), (7, 2)],
+             delays=[delay_rng(dout_delay, dout_delay)])
 
     sim(outdir=tmpdir)
 
 
-# @pytest.mark.parametrize('din_delay', [0, 1])
-# @pytest.mark.parametrize('cfg_delay', [0, 1])
-# @pytest.mark.parametrize('dout_delay', [0, 1])
-# def test_queue_directed(tmpdir, sim_cls, din_delay, cfg_delay, dout_delay):
-#     t_ctrl = Uint[2]
-#     t_din = Queue[Uint[8]]
+@pytest.mark.parametrize('din_delay', [0, 1])
+@pytest.mark.parametrize('cfg_delay', [0, 1])
+@pytest.mark.parametrize('dout_delay', [0, 1])
+@pytest.mark.parametrize('branches', [2, 5, 7])
+def test_mapped_directed(tmpdir, sim_cls, din_delay, cfg_delay, dout_delay,
+                         branches):
 
-#     dut = get_dut(dout_delay)
-#     directed(
-#         drv(t=t_ctrl, seq=[2, 1, 0, 0, 2, 1])
-#         | delay_rng(cfg_delay, cfg_delay),
-#         drv(t=t_din, seq=[[1, 2, 3], [4, 5, 6]])
-#         | delay_rng(din_delay, din_delay),
-#         drv(t=t_din, seq=[[7, 8], [1, 2]])
-#         | delay_rng(din_delay, din_delay),
-#         drv(t=t_din, seq=[[2], [3]])
-#         | delay_rng(din_delay, din_delay),
-#         f=dut(sim_cls=sim_cls),
-#         ref=[(258, 2), (7, 1), (264, 1), (1, 0), (2, 0), (259, 0), (4, 0),
-#              (5, 0), (262, 0), (259, 2), (1, 1), (258, 1)],
-#         delays=[delay_rng(dout_delay, dout_delay)])
+    t_ctrl = Uint[bitw(branches - 1)]
 
-#     sim(outdir=tmpdir)
+    ref = [(i, i) for i in range(branches)]
+
+    mapping = {}
+    for i in range(branches):
+        mapping[i] = (i + 1) if (i + 1) < branches else 0
+
+    ctrl = list(range(branches))
+
+    seqs = [[(i - 1) if (i - 1) >= 0 else (branches - 1)]
+            for i in range(branches)]
+
+    drvs = [
+        drv(t=Uint[s[0] + 1], seq=s) | delay_rng(din_delay, din_delay)
+        for s in seqs
+    ]
+
+    # print(mapping)
+    # print(seqs)
+    # print(ctrl)
+    # print(ref)
+
+    directed(drv(t=t_ctrl, seq=ctrl) | delay_rng(cfg_delay, cfg_delay),
+             *drvs,
+             f=mux(mapping=mapping, sim_cls=sim_cls),
+             delays=[delay_rng(dout_delay, dout_delay)],
+             ref=ref)
+
+    sim(outdir=tmpdir)
+
+
+# test_mapped_directed('/tools/home/tmp', None, 0, 0, 0, 5)
 
 
 @pytest.mark.parametrize('din_delay', [0, 1])
@@ -78,18 +94,17 @@ def test_diff_inputs(tmpdir, sim_cls, din_delay, cfg_delay, dout_delay):
     t_din2 = Queue[Uint[8]]
 
     dut = get_dut(dout_delay)
-    directed(
-        drv(t=t_ctrl, seq=[0, 1, 2])
-        | delay_rng(cfg_delay, cfg_delay),
-        drv(t=t_din0, seq=[5])
-        | delay_rng(din_delay, din_delay),
-        drv(t=t_din1, seq=[6])
-        | delay_rng(din_delay, din_delay),
-        drv(t=t_din2, seq=[[8]])
-        | delay_rng(din_delay, din_delay),
-        f=dut(sim_cls=sim_cls),
-        ref=[(5, 0), (6, 1), (264, 2)],
-        delays=[delay_rng(dout_delay, dout_delay)])
+    directed(drv(t=t_ctrl, seq=[0, 1, 2])
+             | delay_rng(cfg_delay, cfg_delay),
+             drv(t=t_din0, seq=[5])
+             | delay_rng(din_delay, din_delay),
+             drv(t=t_din1, seq=[6])
+             | delay_rng(din_delay, din_delay),
+             drv(t=t_din2, seq=[[8]])
+             | delay_rng(din_delay, din_delay),
+             f=dut(sim_cls=sim_cls),
+             ref=[(5, 0), (6, 1), (264, 2)],
+             delays=[delay_rng(dout_delay, dout_delay)])
 
     sim(outdir=tmpdir)
 
@@ -100,7 +115,6 @@ def test_diff_inputs(tmpdir, sim_cls, din_delay, cfg_delay, dout_delay):
 # ])
 # def test_formal():
 #     mux(Intf(Uint[4]), Intf(Uint[8]), Intf(Uint[8]))
-
 
 # @formal_check(assumes=[
 #     's_eventually (din0_valid == ctrl_valid && (ctrl_data == 0))',
