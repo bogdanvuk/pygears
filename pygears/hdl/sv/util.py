c928c891a14ed97ebe656c2152d639b15abc207a
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2019-07-21 13:20:18 +0200

    Don't create union types in SV

--- pygears/hdl/sv/util.py
+++ pygears/hdl/sv/util.py
@@ -62,7 +62,8 @@ class SVGenTypeVisitor(TypingVisitorBase):
             if (int(t) > max_len):
                 max_len = int(t)
 
-        if self.depth < self.max_depth:
+        # if self.depth < self.max_depth:
+        if False:
             for i, t in reversed(list(enumerate(type_.args))):
                 field_tmp = f'f{i}'
                 struct_fields.append(f'{self.struct_str} // ({t}, u?)')
@@ -93,7 +94,8 @@ class SVGenTypeVisitor(TypingVisitorBase):
             f'    {self.basic_type} [{bitw(len(type_.args)-1)-1}:0] ctrl; // u{bitw(len(type_.args)-1)}'
         )
 
-        if self.depth < self.max_depth:
+        # if self.depth < self.max_depth:
+        if False:
             struct_fields.append(f'    {middle_parent_context}_t data; // {type_}')
         else:
             struct_fields.append(
