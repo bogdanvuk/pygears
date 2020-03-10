a7ba05565b11c78870709b111907397f36e75925
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2020-03-10 12:28:21 +0100

    Be more carefull when bypassing reinterpret

--- pygears/lib/operators.py
+++ pygears/lib/operators.py
@@ -164,7 +164,11 @@ class AddIntfOperPlugin(IntfOperPlugin):
 @flow_visitor
 class SVRemoveReplicate(RTLGearHierVisitor):
     def reinterpret(self, node):
-        node.bypass()
+        pout = node.out_ports[0]
+        pin = node.in_ports[0]
+
+        if getattr(pin.dtype, 'width', 0) == getattr(pout.dtype, 'width', 0):
+            node.bypass()
 
 
 class RTLReinterpretPlugin(VGenPlugin, SVGenPlugin):
