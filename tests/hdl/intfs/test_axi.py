5b9dccf701b52bec0ac4b0ded517a5a783edc9e4
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2020-04-21 07:34:11 +0200

    Add tests folder

---
+++ tests/hdl/intfs/test_axi.py
@@ -0,0 +1,63 @@
+import os
+from pygears import Intf, find
+from pygears.lib import sdp, rom, shred
+from pygears.typing import Tuple, Uint
+from pygears.hdl import hdlgen
+from pygears.util.fileio import save_file
+from pygears.hdl.ipgen import ipgen
+
+from pygears.hdl.intfs.generate import generate
+
+# ret = rom(Intf(Uint[8]), data=[], dtype=Uint[32], dflt=0xaabbccdd)
+# top = find('/rom')
+# outdir = '/tools/home/tmp/axi_test/ip/rom'
+
+# ipgen(
+#     'vivado',
+#     __file__,
+#     outdir=outdir,
+#     top='/rom',
+#     lang='sv',
+#     prjdir='/tools/home/tmp/axi_test/ipprj',
+#     intf={'s_axi': {
+#         'type': 'axi',
+#         'raddr': 'addr',
+#         'rdata': 'dout'
+#     }})
+
+# ret = sdp(Intf(Tuple[Uint[8], Uint[32]]), Intf(Uint[8]))
+
+# top = find('/sdp')
+# outdir = '/tools/home/tmp/axi_test_19_2/ip/rom'
+
+# ipgen(
+#     'vivado',
+#     __file__,
+#     outdir=outdir,
+#     top='/sdp',
+#     lang='sv',
+#     prjdir='/tools/home/tmp/axi_test/ipprj',
+#     intf={'s_axi': {
+#         'type': 'axi',
+#         'raddr': 'rd_addr',
+#         'rdata': 'rd_data',
+#         'waddr': 'wr_addr_data',
+#         'wdata': 'wr_addr_data'
+#     }})
+
+ret = shred(Intf(Uint[32]))
+
+top = find('/shred')
+outdir = '/tools/home/tmp/axi_test/ip/shred'
+
+ipgen(
+    'vivado',
+    __file__,
+    outdir=outdir,
+    top='/sdp',
+    lang='sv',
+    prjdir='/tools/home/tmp/axi_test/ipprj',
+    intf={'s_axi': {
+        'type': 'axidma',
+        'rdata': 'din'
+    }})
