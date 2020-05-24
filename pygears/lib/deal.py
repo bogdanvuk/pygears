7dafaedd9dc69cbac39166296a55b320ce45aeee
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2020-05-24 08:36:28 +0200

    Add different flavor qdeal

--- pygears/lib/deal.py
+++ pygears/lib/deal.py
@@ -1,4 +1,4 @@
-from pygears import gear
+from pygears import gear, alternative
 from pygears.typing import Queue, Uint, Union, bitw
 from .rng import qrange
 from .flatten import flatten
@@ -6,9 +6,9 @@ from .ccat import ccat
 from .demux import demux
 
 
-@gear(hdl={'compile': True})
-async def qdeal(
-        din: Queue, *, num, lvl=b'din.lvl-1') -> b'(Queue[din.data, din.lvl-1], ) * num':
+@gear(hdl={'compile': True}, enablement=b'lvl < din.lvl')
+async def qdeal(din: Queue, *, num,
+                lvl=b'din.lvl-1') -> b'(Queue[din.data, din.lvl-1], ) * num':
 
     i = Uint[bitw(num)](0)
 
@@ -17,13 +17,37 @@ async def qdeal(
 
         dout = data if lvl == 0 else (data, out_eot)
 
-        yield demux(i, dout, use_dflt=False, mapping={n: n for n in range(num)})
+        yield demux(i,
+                    dout,
+                    use_dflt=False,
+                    mapping={n: n
+                             for n in range(num)})
 
         if all(out_eot):
             if i == (num - 1):
                 i = 0
             else:
                 i += 1
+
+
+@alternative(qdeal)
+@gear(hdl={'compile': True}, enablement=b'lvl == din.lvl')
+async def qdeal_same_lvl(din: Queue, *, num,
+                         lvl=b'din.lvl-1') -> b'(din, ) * num':
+
+    i = Uint[bitw(num)](0)
+
+    while i != num:
+        async for (data, eot) in din:
+            dout = data if lvl == 0 else (data, eot)
+
+            yield demux(i,
+                        dout,
+                        use_dflt=False,
+                        mapping={n: n
+                                 for n in range(num)})
+
+        i += 1
 
 
 @gear
