afa649e16bd00066ec8417f0864cd462d87630b3
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2020-11-03 18:39:47 +0100

    Factor demux out of qdeal

--- pygears/lib/deal.py
+++ pygears/lib/deal.py
@@ -7,21 +7,17 @@ from .demux import demux
 
 
 @gear(hdl={'compile': True}, enablement=b'lvl < din.lvl')
-async def qdeal(din: Queue, *, num,
-                lvl=b'din.lvl-1') -> b'(Queue[din.data, din.lvl-1], ) * num':
+async def qdeal_impl(din: Queue, *, num,
+                     lvl=b'din.lvl-1') -> b'Union[(Queue[din.data, din.lvl-1], ) * num]':
 
-    i = Uint[bitw(num)](0)
+    i = Uint[bitw(num - 1)](0)
 
     async for (data, eot) in din:
         out_eot = eot[:lvl]
 
-        dout = data if lvl == 0 else (data, out_eot)
+        d = data if lvl == 0 else (data, out_eot)
 
-        yield demux(i,
-                    dout,
-                    use_dflt=False,
-                    mapping={n: n
-                             for n in range(num)})
+        yield (d, i)
 
         if all(out_eot):
             if i == (num - 1):
@@ -30,11 +26,11 @@ async def qdeal(din: Queue, *, num,
                 i += 1
 
 
-@gear(hdl={'compile': True})
-async def qdeal_same_lvl_impl(din: Queue, *, num,
-                              lvl=b'din.lvl-1') -> b'Union[(din, ) * num]':
+@alternative(qdeal_impl)
+@gear(hdl={'compile': True}, enablement=b'lvl == din.lvl')
+async def qdeal_impl_same_lvl(din: Queue, *, num, lvl=b'din.lvl-1') -> b'Union[(din, ) * num]':
 
-    i = Uint[bitw(num)](0)
+    i = Uint[bitw(num - 1)](0)
 
     while i != num:
         async for (data, eot) in din:
@@ -44,12 +40,11 @@ async def qdeal_same_lvl_impl(din: Queue, *, num,
 
         i += 1
 
-@alternative(qdeal)
-@gear(enablement=b'lvl == din.lvl')
-def qdeal_same_lvl(din: Queue, *, num,
-                         lvl=b'din.lvl-1'):
+
+@gear
+def qdeal(din: Queue, *, num, lvl=b'din.lvl-1'):
     return din \
-        | qdeal_same_lvl_impl(num=num, lvl=lvl) \
+        | qdeal_impl(num=num, lvl=lvl) \
         | demux(use_dflt=False,
                 mapping={n: n
                          for n in range(num)})
