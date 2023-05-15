15b7f0285968af20df9a03981e093505f59c2501
bogdan <>
2023-05-15 15:52:17 +0200

    Allow for external memory structure to be passed to tdp

--- pygears/lib/tdp.py
+++ pygears/lib/tdp.py
@@ -10,47 +10,44 @@ TWrReq = Tuple[{'addr': Uint['w_addr'], 'data': 'data'}]
 TUniReq = Union[Uint['w_addr'], TWrReq]
 
 
-def tdp_port0_setup(module):
-    module.ram = {}
+@gear
+async def tdp_port0(req, *, depth, mem) -> b'req["data"].data':
+    (addr, (data, ctrl)) = await req.get()
+    if ctrl:
+        mem[int(addr)] = data
+    else:
+        dout = mem[int(addr)]
+        await clk()
+        yield dout
 
 
-@gear(sim_setup=tdp_port0_setup)
-async def tdp_port0(req, *, depth) -> b'req["data"].data':
-    ram = module().ram
-
-    async with req as (addr, (data, ctrl)):
-        if ctrl:
-            ram[addr] = data
-        else:
-            yield ram[addr]
-
-
-def tdp_port1_setup(module):
-    module.port0 = find('../tdp_port0')
-
-
-@gear(sim_setup=tdp_port1_setup)
-async def tdp_port1(req, *, depth) -> b'req["data"].data':
-    ram = module().port0.ram
-
-    async with req as (addr, (data, ctrl)):
-        if ctrl:
-            ram[addr] = data
-        else:
-            yield ram[addr]
+@gear
+async def tdp_port1(req, *, depth, mem) -> b'req["data"].data':
+    (addr, (data, ctrl)) = await req.get()
+    if ctrl:
+        mem[int(addr)] = data
+    else:
+        dout = mem[int(addr)]
+        await clk()
+        yield dout
 
 
 @gear(hdl={'hierarchical': False})
 def tdp(
-        req0: TReq,
-        req1: TReq,
-        *,
-        depth=b'2**w_addr',
-        w_data=b'data.width',
-        w_addr=b'w_addr') -> b'(req0["data"].data, req1["data"].data)':
+    req0: TReq,
+    req1: TReq,
+    *,
+    depth=b'2**w_addr',
+    w_data=b'data.width',
+    w_addr=b'w_addr',
+    mem=None,
+) -> b'(req0["data"].data, req1["data"].data)':
 
-    dout0 = req0 | tdp_port0(depth=depth)
-    dout1 = req1 | tdp_port1(depth=depth)
+    if mem is None:
+        mem = {}
+
+    dout0 = req0 | tdp_port0(depth=depth, mem=mem)
+    dout1 = req1 | tdp_port1(depth=depth, mem=mem)
 
     return dout0, dout1
 
